4b5434b98ae6cb5437379bdabadf244e545bd623
Wendell <w@wendellrodrigues.com>
2022-11-17 17:01:21 -0800

    Add everything

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import naterobinson from "./images/naterobinson.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <img src={naterobinson} />
+    </div>
+  );
+}
+
+export default App;
